53ea01542e31c46cd4cde2dd211f8dc1ff119a9d
belle hsieh <hsiehannabelle2024@gmail.com>
2025-12-05 12:28:16 -0500

    added pages, components, etc for app

--- dishcord/client/src/App.js
+++ dishcord/client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { teal, lime, amber, grey } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
 
-function App() {
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import AlbumsPage from './pages/AlbumsPage';
+import SongsPage from './pages/SongsPage';
+import AlbumInfoPage from './pages/AlbumInfoPage'
+
+export const theme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: lime,
+    warning: amber,
+    background: { default: grey[50] },
+  },
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/albums" element={<AlbumsPage />} />
+          <Route path="/albums/:album_id" element={<AlbumInfoPage />} />
+          <Route path="/songs" element={<SongsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
-export default App;
